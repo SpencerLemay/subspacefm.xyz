771d64b0290b2986dc1917915cd199672f1d4f7b
Spencer <lemayspencer@gmail.com>
2022-03-21 18:10:54 -0500

    lets try node again

---
+++ scripts/player.js
@@ -0,0 +1,33 @@
+import { IcecastMetadataReader } from ("icecast-metadata-js");
+
+const icecastReader = new IcecastMetadataReader({
+  onStream: (value) => {
+    // do something with the data in value.stream
+  },
+  onMetadata: (value) => {
+    // do something with the data in value.metadata
+  };,
+});
+
+const headers = myHTTPResponse.headers;
+
+const icecastReader = new IcecastMetadataReader({
+  onStream,
+  onMetadata,
+  onError,
+  enableLogging: true,
+  metadataTypes: ["icy"]
+  icyMetaInt: parseInt(headers.get("Icy-MetaInt")),
+});
+
+
+const responseData = response.body;
+
+for (const i of icecastReader.iterator(responseData)) {
+  if (i.stream) {
+    // do something with stream data
+  }
+  if (i.metadata) {
+    console.log(i.metadata);
+  }
+}
