9909f0cf41f26d83cfc0d371c13f069cc5721e24
Spencer <lemayspencer@gmail.com>
2022-03-20 21:45:32 -0500

    wtf is this shit

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,6 @@
-main.js
+import IcecastMetadataPlayer from "icecast-metadata-player";
+
+const player = new IcecastMetadataPlayer(
+  "https://dsmrad.io/stream/isics-all",
+  { onMetadata: (metadata) => {console.log(metadata)} }
+);
