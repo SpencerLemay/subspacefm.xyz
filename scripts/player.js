8e3f74e6bdf09fad21913d5b43be7c0991b7bb26
Spencer <lemayspencer@gmail.com>
2022-03-21 21:13:59 -0500

    whatever

--- scripts/player.js
+++ scripts/player.js
@@ -1,8 +1,6 @@
 debugger;
 const { IcecastMetadataReader } = require("icecast-metadata-js");
 
-const headers = myHTTPResponse.headers;
-
 const icecastReader = new IcecastMetadataReader({
   onStream,
   onMetadata,
