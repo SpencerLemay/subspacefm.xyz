2dc8644e2d5df5a56b6156decf8a785ff1da5227
Spencer <lemayspencer@gmail.com>
2022-03-22 17:39:32 -0500

    lolk

--- scripts/player.js
+++ scripts/player.js
@@ -1,40 +1,7 @@
 debugger;
-import { IcecastMetadataReader } from "icecast-metadata-js";
+import IcecastMetadataPlayer from "icecast-metadata-player";
 
-const icecastReader = new IcecastMetadataReader({
-  metadataTypes: ["icy", "ogg"]
-});
-
-
-
-
-fetch("https://subspacefm.xyz/stream", {
-  method: "GET",
-  headers: {
-    "Icy-MetaData": "1",
-  }
-})
-.then(async (response) => {
-  const icecast = new IcecastReadableStream(
-    response,
-    options // see IcecastMetadataReader
-  );
-  
-  await icecast.startReading();
-});
-
-
-
-
-const responseData = response.body;
-
-for (const i of icecastReader.iterator(responseData)) {
-  if (i.stream) {
-    // do something with stream data
-   console.log(i.stream);
-  }
-  if (i.metadata) {
-    // do something with metadata
-    console.log(i.metadata);
-  }
-}
+const player = new IcecastMetadataPlayer(
+  "https://subspacefm.xyz/stream",
+  { onMetadata: (metadata) => {console.log(metadata)} }
+);
