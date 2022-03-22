bcfe981c8fbf07eb4e462571fbad369c116b955f
Spencer <lemayspencer@gmail.com>
2022-03-22 17:20:33 -0500

    forgot

--- scripts/bundle.js
+++ scripts/bundle.js
@@ -1174,7 +1174,7 @@ debugger;
 const icecastReader = new _icecastMetadataJs.IcecastMetadataReader({
   metadataTypes: ["icy", "ogg"]
 });
-fetch("https://stream.subspacefm.xyz", {
+fetch("https://subspacefm.xyz/stream", {
   method: "GET",
   headers: {
     "Icy-MetaData": "1"
