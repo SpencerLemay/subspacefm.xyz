e3f37ce496eb6e7cc1348dbffd2d50f53e145e24
Spencer <lemayspencer@gmail.com>
2022-03-22 17:48:19 -0500

    jinx let's see if it works first try

--- scripts/player.js
+++ scripts/player.js
@@ -1,7 +1,18 @@
 debugger;
 import IcecastMetadataPlayer from "icecast-metadata-player";
+var jsdom = require("jsdom").jsdom;
+global.$ = require('jquery/dist/jquery')(jsdom().createWindow());
 
 const player = new IcecastMetadataPlayer(
   "https://subspacefm.xyz/stream",
   { onMetadata: (metadata) => {console.log(metadata)} }
 );
+
+
+$("play").click(function(){
+  player.play();
+}); 
+
+$("stop").click(function(){
+  player.stop();
+}); 
