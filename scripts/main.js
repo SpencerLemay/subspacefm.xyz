811488368fb61032eef604cfcce7c7aa4f32ee26
Spencer <lemayspencer@gmail.com>
2022-03-22 21:34:49 -0500

    visualizer

--- scripts/main.js
+++
@@ -1,6 +0,0 @@
-import IcecastMetadataPlayer from "icecast-metadata-player";
-
-const player = new IcecastMetadataPlayer(
-  "https://dsmrad.io/stream/isics-all",
-  { onMetadata: (metadata) => {console.log(metadata)} }
-);
