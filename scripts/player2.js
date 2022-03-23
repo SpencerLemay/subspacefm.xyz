322d92697b7bf6c180b1acf8d13a304c7c4a15c0
Spencer <lemayspencer@gmail.com>
2022-03-22 19:19:42 -0500

    derp

--- scripts/player2.js
+++ scripts/player2.js
@@ -1,5 +1,3 @@
-import IcecastMetadataPlayer from "icecast-metadata-player";
-
 const player = new IcecastMetadataPlayer(
   "https://subspacefm.xyz/stream",
   { onMetadata: (metadata) => {console.log(metadata)} }
