34a2b8ef4c6fe0df7666b7fbd7bdc28f50a0a966
Spencer <lemayspencer@gmail.com>
2022-04-02 15:54:38 -0500

    test

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 // Entry Point of the API Server 
   
 const express = require('express');
-const IcecastMetadataStats = require("icecast-metadata-stats");
+const IcecastMetadataStats = require("./IcecastMetadataStats.js");
 
 const statsListener = new IcecastMetadataStats("https://subspacefm.xyz/stream", {
   onStats: (stats) => { console.log(stats) },
