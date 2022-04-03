54a508fa04959d538cbb33710dc9c260f045d874
Spencer <lemayspencer@gmail.com>
2022-04-02 22:02:58 -0500

    remove name popup

--- scripts/bundle.js
+++ scripts/bundle.js
@@ -7,9 +7,9 @@ const messageContainer = document.getElementById('message-container')
 const messageForm = document.getElementById('send-container')
 const messageInput = document.getElementById('message-input')
 
-const name = prompt('What is your name?')
-appendMessage('You joined')
-socket.emit('new-user', name)
+//const name = prompt('What is your name?')
+/*var name = 'default';
+appendMessage('Enter Name')*/
 
 socket.on('chat-message', data => {
   appendMessage(`${data.name}: ${data.message}`)
@@ -25,6 +25,12 @@ socket.on('user-disconnected', name => {
 
 messageForm.addEventListener('submit', e => {
   e.preventDefault()
+  
+  if  (name=== 'default'){
+        name = messageInput.value 
+        socket.emit('new-user', name) 
+        appendMessage('Welcome  ' +  name); 
+       }
   const message = messageInput.value
   appendMessage(`You: ${message}`)
   socket.emit('send-chat-message', message)
