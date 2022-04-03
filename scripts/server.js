8e81320c375c3ebf4c92ba1c483689a4349a1eac
Spencer <lemayspencer@gmail.com>
2022-04-03 13:02:05 -0500

    test

--- scripts/server.js
+++ scripts/server.js
@@ -6,11 +6,11 @@ const users = {}
 
 }*/
 
-function getSession() {
+function uuidv4() {
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
     var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-    return v.toString(16);  
-})
+    return v.toString(16);
+  });
 }
 
 function getUserName(username) {
@@ -30,7 +30,7 @@ io.on('connection', socket => {
    
   //isBanned(socket);
 
-   var sessionid = getSession();
+   var sessionid = uuidv4();
    var username = getUserName();
     var user = {
       name: name;
