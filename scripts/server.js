a436c93d5b7b720414af14c1a5fad9102ebdb4c8
Spencer <lemayspencer@gmail.com>
2022-04-02 19:07:46 -0500

    test

--- scripts/server.js
+++ scripts/server.js
@@ -6,11 +6,15 @@ io.on('connection', socket => {
   socket.on('new-user', name => {
     users[socket.id] = name
     socket.broadcast.emit('user-connected', name)
+    console.log("USER CONNECTED: " + name)
   })
   socket.on('send-chat-message', message => {
     socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
+    console.log("USER SAID: " + message)
+
   })
   socket.on('disconnect', () => {
+  	console.log("DISCONNECTED "+ users[socket.id])
     socket.broadcast.emit('user-disconnected', users[socket.id])
     delete users[socket.id]
   })
